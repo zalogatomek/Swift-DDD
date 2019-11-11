c0a603b3f4be9c8dcbc715ca0ced1260d3e8aed6
Tomasz Załoga <>
2019-11-11 21:34:37 +0100

    Create domain for Contact

--- SwiftDDD.playground/Contents.swift
+++ SwiftDDD.playground/Contents.swift
@@ -1,1 +1,97 @@
+import Foundation
 
+// Typical Contact Model
+
+struct ContactTypical {
+    let firstName: String
+    let middleInitial: String?
+    let lastName: String
+    let emailAddress: String
+    let isEmailVerified: Bool
+}
+
+// Step 1 - Wrap primitive types
+
+struct Name: CustomStringConvertible {
+    private static let maxLength: Int = 50
+    let value: String
+    
+    init?(string: String?) {
+        guard let string = string, string.count < Name.maxLength else { return nil }
+        value = string
+    }
+    
+    var description: String {
+        return value
+    }
+}
+
+struct Initial {
+    private static let lenght: Int = 1
+    let value: String
+    
+    init?(string: String?) {
+        guard let string = string, string.count == Initial.lenght else { return nil }
+        value = string
+    }
+    
+    var description: String {
+        return value
+    }
+}
+
+struct Email: CustomStringConvertible {
+    private static let emailRegex = "[^@]+@[^\\.]+\\..+"
+    let value: String
+    
+    init?(string: String?) {
+        guard let email = string, Email.isValidEmail(email: email) else { return nil }
+        self.value = email
+    }
+    
+    private static func isValidEmail(email: String) -> Bool {
+        return email.range(of: emailRegex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+    
+    var description: String {
+        return value
+    }
+}
+
+struct ContactWrappedTypes {
+    let firstName: Name
+    let middleInitial: Initial?
+    let lastName: Name
+    let emailAddress: Email
+    let isEmailVerified: Bool
+}
+
+// Step 2 - Group Linked Data
+
+struct PersonalName {
+    let firstName: Name
+    let middleInitial: Initial?
+    let lastName: Name
+}
+
+struct EmailContactInfo {
+    let emailAddress: Email
+    let isEmailVerified: Bool
+}
+
+struct ContactLinkedData {
+    let name: PersonalName
+    let email: EmailContactInfo
+}
+
+// Step 3 - Encoding domain logic
+
+enum EmailInfo {
+    case verfied(Email)
+    case unverified(Email)
+}
+
+struct Contact {
+    let name: PersonalName
+    let email: EmailInfo
+}
